bfc799113a8f1ebe6c3e80b378e19deb87a0a5ed
liciolentimo <lentimo@gmail.com>
2019-04-23 09:38:02 +0300

    Get FCM token

--- app/src/main/java/com/lentimosystems/licio/androidnotifications2/MainActivity.java
+++ app/src/main/java/com/lentimosystems/licio/androidnotifications2/MainActivity.java
@@ -3,17 +3,26 @@ package com.lentimosystems.licio.androidnotifications2;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String CHANNEL_ID = "lentimo_systems";
     private static final String CHANNEL_NAME = "Lentimo Systems";
     private static final String CHANNEL_DESC = "Lentimo Systems Notifications";
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +37,27 @@ public class MainActivity extends AppCompatActivity {
             manager.createNotificationChannel(channel);
         }
 
-        findViewById(R.id.btnNotify).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                displayNotification();
-            }
-        });
+        textView = findViewById(R.id.txtNotify);
+
+//        findViewById(R.id.btnNotify).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                displayNotification();
+//            }
+//        });
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (task.isSuccessful()){
+                            String token = task.getResult().getToken();
+                            textView.setText("Token: " + token);
+                        } else {
+                            textView.setText(task.getException().getMessage());
+                        }
+                    }
+                });
     }
 
     private void displayNotification(){
