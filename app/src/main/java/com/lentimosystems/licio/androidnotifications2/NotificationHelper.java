f494880b0bd32775fdadf56ad21f4265f84aadfa
liciolentimo <lentimo@gmail.com>
2019-05-02 18:25:16 +0300

    Handling clicks on notification to open in app

--- app/src/main/java/com/lentimosystems/licio/androidnotifications2/NotificationHelper.java
+++ app/src/main/java/com/lentimosystems/licio/androidnotifications2/NotificationHelper.java
@@ -1,6 +1,8 @@
 package com.lentimosystems.licio.androidnotifications2;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 
@@ -9,10 +11,22 @@ import static com.lentimosystems.licio.androidnotifications2.MainActivity.CHANNE
 public class NotificationHelper {
 
     public static void displayNotification(Context context, String title, String body){
+
+        Intent intent = new Intent(context, ProfileActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                context,
+                100,
+                intent,
+                PendingIntent.FLAG_CANCEL_CURRENT
+        );
+
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context,CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_chat_bubble_outline_black_24dp)
                 .setContentTitle(title)
                 .setContentText(body)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
         NotificationManagerCompat managerCompat = NotificationManagerCompat.from(context);
